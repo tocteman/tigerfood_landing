db48ff4c76ca1e9117d440d031101804caab35e9
tocteman <juanfrancisco.vinueza@gmail.com>
2021-05-10 03:08:48 -0500

    quitar disque prefetch

--- src/controllers/food_types_controller.js
+++ src/controllers/food_types_controller.js
@@ -5,9 +5,6 @@ export default class extends Controller {
   static targets = [ "selectFood", "conejo", "thePit" ]
   connect() {
     annotate(this.selectFoodTarget, {type: 'underline', color: '#eedadf' }).show()
-    fetch("https://tigerfood.netlify.app/partials/food_type/conejo")
-      .then(()=> console.log("ok"))
-      .catch(err => console.log(err))
   }
 
 
