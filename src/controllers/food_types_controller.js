0d97f400a3680abc6a80203c7c50b80dd248a85e
tocteman <juanfrancisco.vinueza@gmail.com>
2021-05-10 01:21:36 -0500

    src?

--- src/controllers/food_types_controller.js
+++ src/controllers/food_types_controller.js
@@ -2,11 +2,9 @@ import { Controller } from "stimulus"
 import { annotate } from "rough-notation"
 
 export default class extends Controller {
-  static targets = [ "selectFood", "conejoFood", "thePit" ]
+  static targets = [ "selectFood", "conejo", "thePit" ]
   connect() {
     annotate(this.selectFoodTarget, {type: 'underline', color: '#eedadf' }).show()
-    const pitElement = this.thePitTarget
-    console.log("the Pit Element:")
   }
 
 
