6e1c3832192bebc55c60e3608d5739a1ff7a4e94
tocteman <juanfrancisco.vinueza@gmail.com>
2021-05-17 00:30:26 -0500

    la tabla

--- src/controllers/main_paper_controller.js
+++ src/controllers/main_paper_controller.js
@@ -2,15 +2,15 @@ import { Controller } from "stimulus"
 import { annotate } from "rough-notation"
 
 export default class extends Controller {
-  static targets = ["phone", "whatsappNumber" ]
+  static targets = ["phone", "whatsappNumber", "greatDivider"]
   connect(){
+    annotate(this.greatDividerTarget, {type: 'underline', color: '#9070af'}).show()
     this.whatsappNumberTarget.href =/Mobi/.test(navigator.userAgent) ?
       "whatsapp://send?text=&phone=+593998478479" :
       "/#footer"
   }
   highlight(){
-    const resaltado = annotate(this.phoneTarget, {type: 'circle', color: '#6EE7B7'})
-    resaltado.show()
+    annotate(this.phoneTarget, {type: 'circle', color: '#6EE7B7'}).show()
     setTimeout(() => resaltado.hide(), 2500)
   }
 }
